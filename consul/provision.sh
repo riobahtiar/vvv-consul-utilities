e3f991c3f46f5a5accab93d04e96a127049c3e4a
Rio Bahtiar <akhibahtiar@gmail.com>
2018-10-30 22:51:00 +0700

    add sudo capabilities

--- consul/provision.sh
+++ consul/provision.sh
@@ -5,12 +5,12 @@ echo "## Prepare to Install Consul ##"
 package_install() {
 	# Install unzip
 	echo -e "\nInstalling Unzip.."
-	apt-get -y install unzip
+	sudo apt-get -y install unzip
 	# Install Consul Node..
 	if [[ ! -f "/usr/local/bin/consul" ]]; then
 		echo -e "\nDownloading Consul 1.3.0, consul_1.3.0_linux_amd64.zip"
 		wget https://releases.hashicorp.com/consul/1.3.0/consul_1.3.0_linux_amd64.zip
-		unzip consul_1.3.0_linux_amd64.zip -d /usr/local/bin
+		sudo unzip consul_1.3.0_linux_amd64.zip -d /usr/local/bin
 		rm -rf consul_1.3.0_linux_amd64.zip
 	else
 		echo "Consul already installed."
@@ -18,7 +18,7 @@ package_install() {
 }
 
 configure() {
-	service consul restart
+	sudo service consul restart
 }
 
 package_install
