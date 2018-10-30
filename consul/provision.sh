e8247d06dc62b750a6c23de677a7e889c84d5d8d
Rio Bahtiar <akhibahtiar@gmail.com>
2018-10-30 22:23:44 +0700

    rewrite code with bash func

--- consul/provision.sh
+++ consul/provision.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
-if [[ -f /usr/bin/consul ]]; then
-	echo "Consul is already installed."
-	exit 0
-fi
-
-echo "Installing Consul"
+echo "## Prepare to Install Consul ##"
 
 # Install prerequisites.
-apt-get install unzip &>/dev/null
-wget https://releases.hashicorp.com/consul/1.3.0/consul_1.3.0_linux_amd64.zip
-unzip consul_1.3.0_linux_amd64.zip -d /usr/local/bin
-rm -rf consul_1.3.0_linux_amd64.zip
+package_install() {
+	# Install unzip
+	echo -e "\nInstalling Unzip.."
+	apt-get -y install unzip
+	# Install Consul Node..
+	if [[ ! -f "/usr/local/bin/consul" ]]; then
+		echo -e "\nDownloading Consul 1.3.0, consul_1.3.0_linux_amd64.zip"
+		wget https://releases.hashicorp.com/consul/1.3.0/consul_1.3.0_linux_amd64.zip
+		unzip consul_1.3.0_linux_amd64.zip -d /usr/local/bin
+		rm -rf consul_1.3.0_linux_amd64.zip
+	else
+		echo "Consul already installed."
+	fi
+}
+
+configure() {
+	service consul restart
+}
+
+package_install
+configure
